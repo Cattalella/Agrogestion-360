fce467ab709c46b6227e349f5034ce5e6a37bd88
itsnohava <nohavaevelyn05@gmail.com>
2026-03-04 10:54:07 -0500

    admin

---
+++ src/pages/Admin.tsx
@@ -0,0 +1,24 @@
+import { Navbar } from "../components/Navbar";
+import fondo from "../assets/imgs/Encabezado_admin.webp";
+import plantas from "../assets/imgs/PLANTAS.webp";
+import usuario from "../assets/imgs/icon_user.webp";
+import reloj from "../assets/imgs/icon_reloj.webp";
+import vaca from "../assets/imgs/icon_vaca.webp";
+import vacuna from "../assets/imgs/icon_vacuna.webp";
+import venta from "../assets/imgs/icon_ventas.webp";
+import sack from "../assets/imgs/icon_sack.webp";
+import martillo from "../assets/imgs/icon_martillo.webp";
+import trabajadores from "../assets/imgs/icon_trabajadores.webp";
+import compra from "../assets/imgs/icon_compra.webp";
+import documento from "../assets/imgs/icon_documento.webp";
+import insumos from "../assets/imgs/icon_insumo.webp";
+import inventario from "../assets/imgs/icon_inventario.webp";
+import { div } from "framer-motion/client";
+
+export const hero = () => {
+    return (
+        <div className="">
+            <Navbar />
+        </div>
+    )
+}
