bdce01d4c941250c520dce5c03e71d59f43d9651
Cesar Cortés <edithcattalella@gmail.com>
2026-03-04 09:56:34 -0500

    git ignore

--- AGROGESTION-360-REACT-TSX-main/src/components/Botones.tsx
+++
@@ -1,9 +0,0 @@
-const Botones = ({ texto, estilo, onClick }: any) => {
-    return (
-    <button className={estilo} onClick={onClick}>
-        {texto}
-    </button>
-    );
-};
-
-export default Botones;
